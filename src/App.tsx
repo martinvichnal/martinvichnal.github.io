01244d723bf4e0dc520d51d7fb5bf15537696ba9
vmazser <vmazsit@gmail.com>
2023-08-26 14:23:29 +0200

    Added Banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 
 import { Navbar } from './components/Navbar';
+import { Banner } from './components/Banner';
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
-
+      <Banner />
     </>
   );
 }
