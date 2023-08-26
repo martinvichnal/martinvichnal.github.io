a8f014723b09c438bba112b74d0c42e4d89e6099
vmazser <vmazsit@gmail.com>
2023-08-26 12:27:24 +0200

    Added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+    </>
   );
 }
 
