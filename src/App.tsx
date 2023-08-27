306ff753232896191b1f850b05187cd182a3cfda
vmazser <vmazsit@gmail.com>
2023-08-27 13:20:16 +0200

    Installed flowbite-react

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import Footer from './components/Footer';
-import Analytics from './components/Analytics';
-import Newsletter from './components/Newsletter';
-import Cards from './components/Cards';
-
 
 function App() {
 
@@ -14,9 +10,6 @@ function App() {
     <>
       <Navbar />
       <Intro />
-      {/* <Analytics />
-      <Newsletter />
-      <Cards /> */}
       <Footer />
     </>
   );
