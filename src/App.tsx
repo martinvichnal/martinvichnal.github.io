b01ed113a68443f7e20284c89c29a4ed0dad6148
vmazser <vmazsit@gmail.com>
2023-08-31 18:07:30 +0200

    v1.1
    v1.1 - Project Update
    Added:
    - An object to store all projects.
    - Dynamic rendering projects with .map function
    - Dynamic rendering project tags within the project card with predefined tags.
    - Basic Filtering feature with Projects (Not functional at the moment)
    - Pomodoro Cover for the projects card
    
    Updated:
    - README.md

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,44 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import FooterMain from './components/FooterMain';
-import { Projects } from './components/Projects';
+import Projects, { projectObj, allTags } from './components/Projects';
+import { useState } from 'react';
+
+let filterTags = allTags;
 
 function App() {
+  // This will be used for the select tag filter. Will have to search for multy select.
+  const [selectedTags, setSelectedTags] = useState<string>("All");
+  const handleTagChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedTags(event.target.value);
+  };
 
   return (
     <>
       <Navbar />
       <Intro />
-      <Projects />
+      {/* Projects section */}
+      <div className="p-10">
+        <div className="p-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select what type of projects do you want to filter</label>
+          <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="All" selected>All</option>
+            {allTags.map((tag) => (<option value={tag}>{tag}</option>))}
+          </select>
+        </div>
+        <div className="grid gap-4 grid-cols-3">
+          {projectObj.filter(project => project.tags.some(tag => filterTags.includes(tag))).map(filteredProjects => (<Projects
+            key={filteredProjects.title}
+            title={filteredProjects.title}
+            description={filteredProjects.description}
+            image={filteredProjects.image}
+            tags={filteredProjects.tags}
+            webSource={filteredProjects.webSource}
+            gitSource={filteredProjects.gitSource} />
+          ))}
+        </div>
+      </div >
+
       <FooterMain />
     </>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,26 +1,67 @@
-import { Card } from 'flowbite-react';
-import Pomodoro from '../assets/arch.jpeg'
+import pomodoroImage from '../assets/PomodoroCover.png'
+import portfolioImage from '../assets/feels.png'
+import smartLampImage from '../assets/door.jpg'
 
-export const Projects = () => {
+// Projects props
+type ProjectsProps = {
+    title: string,
+    description: string,
+    image: string,
+    tags: string[],
+    webSource: string,
+    gitSource: string
+}
+
+// Exporting all tags for the filter
+export const allTags = ["React", "WebDev", "ESP32", "C++", "IoT"];
+
+// Project object
+export const projectObj = [
+    {
+        title: "Pomodoro tracker",
+        description: "This is my first sort of complex React project. This page is based on the pomodoro technique which is an excellent way for time management. It was invented in the late 1980s by Francesco Cirillo.",
+        image: pomodoroImage,
+        tags: ["React", "WebDev"],
+        webSource: "https://www.martinvichnal.com/pomodoro/",
+        gitSource: "https://github.com/martinvichnal/pomodoro"
+    },
+    {
+        title: "Portfolio",
+        description: "This is my personal portfolio.",
+        image: portfolioImage,
+        tags: ["React", "WebDev"],
+        webSource: "https://www.martinvichnal.com/",
+        gitSource: "https://github.com/martinvichnal/"
+    },
+    {
+        title: "IoT Smart Lamp",
+        description: "Smart lamp built with ESP32 and WS2812B LEDs. It is controlled by a web interface.",
+        image: smartLampImage,
+        tags: ["ESP32", "C++", "WebDev", "IoT"],
+        webSource: "https://github.com/martinvichnal/ESP-IoT-Lamp",
+        gitSource: "https://github.com/martinvichnal/ESP-IoT-Lamp"
+    }
+]
+
+
+const Projects = ({ title, description, image, tags, webSource, gitSource }: ProjectsProps) => {
     return (
         <div>
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="https://www.martinvichnal.com/pomodoro/" target="_blank">
-                    <img className="rounded-t-lg" src={Pomodoro} alt="" />
+                <a href={webSource} target="_blank">
+                    <img className="rounded-t-lg object-cover h-48 w-96" src={image} alt="Project image" />
                 </a>
                 <div className="p-5">
-                    <a href="https://www.martinvichnal.com/pomodoro/" target="_blank">
+                    <a href={webSource} target="_blank">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Pomodoro tracker
+                            {title}
                         </h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        This is my first sort of complex React project. This page is based on the pomodoro technique
-                        which is an excellent way for time management. It was invented in the late 1980s by Francesco Cirillo.
-
+                        {description}
                     </p>
                     <a
-                        href="https://www.martinvichnal.com/pomodoro/"
+                        href={webSource}
                         target="_blank"
                         className="mr-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
@@ -30,7 +71,7 @@ export const Projects = () => {
                         </svg>
                     </a>
                     <a
-                        href="https://github.com/martinvichnal/pomodoro"
+                        href={gitSource}
                         target="_blank"
                         className="mx-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
@@ -41,11 +82,52 @@ export const Projects = () => {
                     </a>
                 </div>
                 <div className="px-5 pb-5">
-                    <span className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">React</span>
-                    <span className="bg-red-100 text-red-800 text-xs font-medium mt-4 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">WebDev</span>
+                    <CreateTags tags={tags} />
                 </div>
             </div>
         </div>
 
     )
+}
+
+export default Projects;
+
+
+// Creating tags with different colors in the projects section.
+// Colors: React: blue, WebDev: red, ESP32: green, C++: yellow, IoT: purple
+const CreateTags = ({ tags }: { tags: string[] }) => {
+    return (
+        <div>
+            {tags.map((tag) => {
+                let tagClassName = '';
+
+                switch (tag) {
+                    case 'React':
+                        tagClassName = 'bg-blue-100 text-blue-800 text-xs dark:bg-blue-900 dark:text-blue-300';
+                        break;
+                    case 'WebDev':
+                        tagClassName = 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300';
+                        break;
+                    case 'ESP32':
+                        tagClassName = 'bg-green-100 text-green-800 rounded dark:bg-green-900 dark:text-green-300';
+                        break;
+                    case 'C++':
+                        tagClassName = 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300';
+                        break;
+                    case 'IoT':
+                        tagClassName = 'bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300';
+                        break;
+                    default:
+                        tagClassName = 'bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300';
+                        break;
+                }
+
+                return (
+                    <span key={tag} className={`text-xs font-medium mt-4 px-2.5 py-0.5 mx-0.5 rounded ${tagClassName}`}>
+                        {tag}
+                    </span>
+                );
+            })}
+        </div>
+    );
 }
